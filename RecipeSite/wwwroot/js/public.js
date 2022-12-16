98efb0c3243f7e0bd7a64c05e5cb9998b94530c1
CCG226 <cown877@gmail.com>
2022-12-15 20:29:29 -0600

    change card style

---
+++ RecipeSite/wwwroot/js/public.js
@@ -0,0 +1,9 @@
+﻿function bookmark(element) {
+    if (element.innerHTML === "bookmark") {
+        element.innerHTML = "bookmark_added";
+    }
+    else (element.innerHTML === "bookmark_added") {
+        element.innerHTML = "bookmark";
+    }
+    
+}
