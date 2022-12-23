88020a59a90439a3d67ab4797f801f105c80eb35
CCG226 <cown877@gmail.com>
2022-12-22 22:08:45 -0600

    working on notification system

--- RecipeSite/wwwroot/js/public.js
+++ RecipeSite/wwwroot/js/public.js
@@ -12,5 +12,31 @@
     
 }
 function bookmarkNotification() {
-    window.alert("sometext");
+    let bookmark = document.getElementById("myBookmark");
+    if (bookmark.innerHTML === "bookmark") {
+        savedNotification();
+        return;
+    }
+
+
+   
+
+    /// const target = document.getElementById("alertDiv");
+//    window.onload = setInterval(() => target.style.opacity = '0', 3000);
+}
+
+async function savedNotification() {
+
+        if ($("#savedAlert").find("div#savedAlert2").length == 0) {
+            $("#savedAlert").append("<div class='alert alert-success alert-dismissable' id='savedAlert2'> <button type='button' class='close' data-dismiss='alert'  aria-hidden='true'>&times;</button> <strong>Saved!</strong> message sent successfully.</div>");
+        }
+    $("#savedAlert").css("display", "");
+    await sleep(2000);
+  //  $("#savedAlert").css("display", "none");
+}
+function unsavedNotification() {
+
+}
+function sleep(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
 }
