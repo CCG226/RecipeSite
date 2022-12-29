f7b5e315bf8650b35997b7fbef70e4ded85d33a5
CCG226 <cown877@gmail.com>
2022-12-28 20:42:01 -0600

    Working on Create Recipe Form

---
+++ RecipeSite/Controllers/UserPageController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecipeSite.Data;
+using RecipeSite.Models;
+
+namespace RecipeSite.Controllers
+{
+    public class UserPageController : Controller
+    {
+        public readonly ApplicationDbContext context;
+
+
+
+        public UserPageController(ApplicationDbContext _context)
+        {
+
+            context = _context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateRecipe()
+        {
+            return View("CreateRecipe");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateRecipe(Recipe Recipe)
+        {
+            return RedirectToAction("CreateRecipe", "UserPage");
+        }
+    }
+}
